4d58928ff2835706cdfa26b667369d9b006cf298
yogendra3236 <yogendra18@navgurukul.org>
2019-05-10 11:01:07 +0530

    modified files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Python-Requests-API
+
+In this project, I have learnt how to make requests using Requests module in Python. I have also saved the list of all the courses in `file.json` file.
+
+## Requirements
+
+### Requests Library
+
+Requests is an Apache2 Licensed HTTP library, written in Python. Requests will allow you to send HTTP/1.1 requests using Python. You can install requests library using following code in your terminal in Linux.
+
+`pip3 install requests`
+
+### Instructions to run the code
+
+If you want to run the code in Linux, open your terminal and type-
+`python3 callingapi.py`
--- callingapi.py
+++ callingapi.py
@@ -1,24 +1,30 @@
 import requests, json, os.path
 from os import path
-exists = path.exists("/home/yogendra/Desktop/RequestsAPI/file.json")
+
+exists = path.exists("/home/yogi/Documents/Python-Requests-API/file.json")
 if exists:
     with open("file.json") as f:
         k = json.load(f)
 else:
     a = requests.get("http://saral.navgurukul.org/api/courses")
     b = a.text # json format
+    b = json.loads(b)
+    b = json.dumps(b, indent=4, sort_keys=True)
     with open("file.json", "w") as f:
         f.write(b)
     k = json.loads(b)
+    print (type(b), type(k))
 l = k["availableCourses"]
+
 def headings():
     for i in range(len(l)):
         print (str(i)+". "+l[i]["name"]) #course-headings
-    print ""
+    print ("")
     exercises()
 
 def exercises():
-    user = input("enter an input: ")
+    user = int(input("enter an index input to choose among the list of courses: "))
+    z = ""
     for i in range(len(l)):
         if user == i:
             value_store = l[i]["name"] # full list
@@ -27,46 +33,46 @@ def exercises():
             var = requests.get("http://saral.navgurukul.org/api/courses/" + str(l[i]["id"]) + "/exercises")
             x = var.text
             y = json.loads(x) # iske andar data hai saara `dict`
-            z = y["data"]
+            z = y["data"] 
     for j in range(len(z)):
         print ("          "+str(j)+". "+z[j]["name"] + "\n")
     #here outside-loop, the value of `j` is maximum
-    user2 = input("enter another input: ")
-    print ""
+    user2 = int(input("enter another input to choose among the list of exercises: ")) # to choose among the list of Courses
+    print ("")
     for i in range(len(z)):
         if user2 <= j: # to check if input given is wrong. Here `j` has its maximum value
             if user2 == i:
                 new_var = requests.get("http://saral.navgurukul.org/api/courses/75/exercise/getBySlug?slug="+str(z[i]["slug"]))
                 # print "http://saral.navgurukul.org/api/courses/75/exercise/getBySlug?slug="+str(z[i]["slug"])
                 access = new_var.text
-                print access + "\n"
+                print (access + "\n")
                 while True:
-                    agla_input = raw_input("enter an input(`p` for previous slug or `n` for next slug or `up` for main-menu or `e` to exit): ")
+                    agla_input = input("enter an input(`p` for previous slug or `n` for next slug or `up` for main-menu or `e` to exit): ")
                     if agla_input == "p":
                         i = i-1
                         if i < 0:
-                            print "your index was out of range:(\n"
+                            print ("your index was out of range:(\n")
                             headings()
                         new_var = requests.get("http://saral.navgurukul.org/api/courses/75/exercise/getBySlug?slug="+str(z[i]["slug"]))
-                        print new_var.text
+                        print (new_var.text)
                         continue
                     elif agla_input == "n":
                         i = i+1
                         if i > j:
-                            print "your index was out of range:(\n"
+                            print ("your index was out of range:(\n")
                             headings()
                         new_var = requests.get("http://saral.navgurukul.org/api/courses/75/exercise/getBySlug?slug="+str(z[i]["slug"]))
-                        print new_var.text
+                        print (new_var.text)
                         continue
                     elif agla_input == "up":
                         headings()
                     elif agla_input == "e":
-                        print "thanks for playing :D"
+                        print ("thanks for playing :D")
                         break
                     else:
-                        print "you entered a wrong input"
+                        print ("you entered a wrong input")
                         break
         else:
-            print "you've given an invalid input\nPlease try again:)\n"
+            print ("you've given an invalid input\nPlease try again:)\n")
             headings()
 headings() # calling the headings function
